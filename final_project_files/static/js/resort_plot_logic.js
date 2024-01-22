29ba0bb5e9080bd94b76d46c7e709ab37494ea95
mj00714 <144157036+mj00714@users.noreply.github.com>
2024-01-22 16:39:22 -0700

    adjustment to legend

--- final_project_files/static/js/resort_plot_logic.js
+++ final_project_files/static/js/resort_plot_logic.js
@@ -168,7 +168,7 @@ function createResortMarkers(data, startPoint, forecast) {
     let legend = L.control({ position: "bottomright" });
     legend.onAdd = function() {
       let div = L.DomUtil.create("div", "info legend");
-      let categories = ['> 50%','< 50%','% Unavailable','No Snow or Forecast Unavailable'];
+      let categories = ['> 50%','< 50%','% Unavailable','No Snow Forecast'];
       let labelColors = ['green', 'yellow', 'blue', 'black'];
 
       let legendInfo = "<h1>Chance of Snow<br /></h1>" +
